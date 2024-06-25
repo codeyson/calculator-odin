d8fad3d374d6e6dc20256af2d83ff0634ea0089f
Fred <frederickdianzon@gmail.com>
2024-06-25 19:02:26 +0800

    The button was connected after unknown errors

--- main.js
+++ main.js
@@ -14,10 +14,24 @@ function divide (num1, num2){
     return num1 / num2;
 }
 
-function calculate () {
-    num1 = parseInt(prompt("Enter num1: "))
-    num2 = parseInt(prompt("Enter num1: "))
-}
+function operate(a, b, operator) {
+    a = +a;
+    b = +b;
+    if (operator === "+") return add(a, b);
+    else if (operator === "-") return subtract(a, b);
+    else if (operator === "x") return multiply(a, b);
+    else if (operator === "/") return divide(a, b);
+    else return null;
+  }
 
-calculate()
-console.log(add(num1, num2));
+
+  const display = document.querySelector(".display");
+
+  const btn = document.querySelector(".buttons");
+
+  function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+
+  btn.addEventListener("click", alertFunction);
+  
