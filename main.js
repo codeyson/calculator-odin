6977f93d024eee5c1537c3e7129d554ba59b9d98
Fred <frederickdianzon@gmail.com>
2024-07-01 00:18:26 +0800

    Project was finish with a help pof a tutorial online. After countless trial n error, it was done!

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+const display = document.querySelector(".display");
+const operand = document.querySelector(".operand")
+
+
+let firstNum;
+let secondNum;
+let step = 0;
+let numArray = [];
+let secondNumArray = [];
+let result = 0
+
+
+function getNum(num) {
+    if (step === 0 || step === 1) {
+        numArray.push(num)
+        step = 1
+        firstNum = Number(numArray.join(''))
+        display.value = firstNum
+        
+    } 
+    else if (step === 2) {
+        secondNumArray.push(num)
+        secondNum = Number(secondNumArray.join(''))
+        display.value = secondNum
+    
+    }
+}
+
+function getOperator(op) {
+    step = 2;
+    operation = op;
+}
+
+function backspace () {
+    if (display.value.length > 0) {
+        display.value = display.value.slice(0, -1);
+    }
+}
+
+function clearDisplay () {
+    display.value = 0;
+    firstNum = null;
+    secondNum = null;
+    step = 0;
+    operation = null;
+    result = 0;
+    numArray = [];
+    secondNumArray = [];
+}
+
 function add (num1, num2){
     return num1 + num2;
 }
@@ -14,22 +64,31 @@ function divide (num1, num2){
     return num1 / num2;
 }
 
-function operate(a, b, operator) {
-    a = +a;
-    b = +b;
-    if (operator === "+") return add(a, b);
-    else if (operator === "-") return subtract(a, b);
-    else if (operator === "x") return multiply(a, b);
-    else if (operator === "/") return divide(a, b);
-    else return null;
-  }
+function percent (num1, num2){
+    return num1 % num2;
+}
 
 
-  const display = document.querySelector(".display");
-  const buttons = document.querySelectorAll(".buttons");
-  
-  buttons.forEach(button => {
-    button.addEventListener("click", event => {
-      display.value += event.target.value;
-    });
-  });
+function calculate () {
+    if (operation === '+') {
+        result = add(firstNum, secondNum);
+        display.value = result
+    }
+    else if (operation === '-') {
+        result = subtract(firstNum, secondNum);
+        display.value = result
+    }
+    else if (operation === '*') {
+        result = multiply(firstNum, secondNum);
+        display.value = result
+    }
+    else if (operation === '/') {
+        result = divide(firstNum, secondNum);
+        display.value = result
+    }
+    else if (operation === '%') {
+        result = percent(firstNum, secondNum);
+        display.value = result
+    }
+}
+
